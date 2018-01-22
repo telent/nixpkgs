c0c20147771d1699a2be33426a818676332b419e
Daniel Barlow <dan@telent.net>
2018-01-22 13:17:36 +0000

    monit: crosscompile, make openssl optional

--- pkgs/tools/system/monit/default.nix
+++ pkgs/tools/system/monit/default.nix
@@ -1,5 +1,9 @@
-{stdenv, fetchurl, openssl, bison, flex, pam, zlib, usePAM ? stdenv.isLinux }:
+{stdenv, fetchurl, openssl, bison, flex, pam, zlib
+, buildPlatform, hostPlatform
+, usePAM ? stdenv.isLinux }:
 
+let useSSL = (openssl != null);
+    cross = ( buildPlatform != hostPlatform ) ; in
 stdenv.mkDerivation rec {
   name = "monit-5.23.0";
 
@@ -9,12 +13,17 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ bison flex ];
-  buildInputs = [ openssl zlib.dev ] ++ stdenv.lib.optionals usePAM [ pam ];
+  buildInputs = [ zlib.dev ] ++
+  stdenv.lib.optionals useSSL [ openssl ] ++
+  stdenv.lib.optionals usePAM [ pam ];
 
-  configureFlags = [
+  configureFlags = ( if useSSL then [
     "--with-ssl-incl-dir=${openssl.dev}/include"
     "--with-ssl-lib-dir=${openssl.out}/lib"
-  ] ++ stdenv.lib.optionals (! usePAM) [ "--without-pam" ];
+  ] else [ "--without-ssl" ] ) ++
+    stdenv.lib.optionals (! usePAM) [ "--without-pam" ] ++
+    stdenv.lib.optionals cross [ "libmonit_cv_setjmp_available=yes"
+                     "libmonit_cv_vsnprintf_c99_conformant=yes"  ]    ;
 
   meta = {
     homepage = http://mmonit.com/monit/;
