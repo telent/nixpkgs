8d28c21d2f6bc035d6cc90dfe0168381f7c94a1e
Daniel Barlow <dan@telent.net>
2018-02-20 16:40:13 +0000

    add mips32 (bigendian)

--- lib/systems/parse.nix
+++ lib/systems/parse.nix
@@ -75,6 +75,7 @@ rec {
     aarch64  = { bits = 64; significantByte = littleEndian; family = "aarch64"; };
     i686     = { bits = 32; significantByte = littleEndian; family = "x86"; };
     x86_64   = { bits = 64; significantByte = littleEndian; family = "x86"; };
+    mips     = { bits = 32; significantByte = bigEndian;    family = "mips"; };
     mips64el = { bits = 32; significantByte = littleEndian; family = "mips"; };
     mipsel =   { bits = 32; significantByte = littleEndian; family = "mips"; };
     powerpc  = { bits = 32; significantByte = bigEndian;    family = "power"; };
